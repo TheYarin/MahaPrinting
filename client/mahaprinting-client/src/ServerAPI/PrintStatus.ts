489fefa1edb8a88615aeb807305434983cb9ca3b
Nitzan <nitzankl27@gmail.com>
2020-04-18 15:14:42 +0300

    Added alt print status list

--- client/mahaprinting-client/src/ServerAPI/PrintStatus.ts
+++ client/mahaprinting-client/src/ServerAPI/PrintStatus.ts
@@ -1,6 +1,16 @@
 export enum PrintStatus {
-  IN_QUEUE = "IN_QUEUE",
-  PRINTING = "PRINTING",
-  DONE = "DONE",
-  CANCELED = "CANCELED",
+    IN_QUEUE = "IN_QUEUE",
+    PRINTING = "PRINTING",
+    DONE = "DONE",
+    CANCELED = "CANCELED",
+}
+
+export enum AltPrintStatus {
+    UPLOADED = "UPLOADED",
+    IN_QUEUE = "IN_QUEUE",
+    PRINTING = "PRINTING",
+    PRINT_PAUSED = "PRINT_PAUSED",
+    PRINT_CANCELLED = "PRINT_CANCELLED",
+    PRINT_FAILED = "PRINT_FAILED",
+    PRINTED = "PRINTED",
 }
