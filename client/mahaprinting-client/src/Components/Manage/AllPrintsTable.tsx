7ba8bb551f9fb28f853d6f358cab6c66f5ea967a
Nitzan <nitzankl27@gmail.com>
2020-04-18 13:15:28 +0300

    Made some sorting/column changes to the prints table

--- client/mahaprinting-client/src/Components/Manage/AllPrintsTable.tsx
+++ client/mahaprinting-client/src/Components/Manage/AllPrintsTable.tsx
@@ -26,73 +26,76 @@ import { observer } from "mobx-react";
 import moment from "moment";
 
 const tableIcons = {
-  Add: forwardRef((props, ref: any) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef((props, ref: any) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref: any) => <Clear {...props} ref={ref} />),
-  Delete: forwardRef((props, ref: any) => <DeleteOutline {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref: any) => <ChevronRight {...props} ref={ref} />),
-  Edit: forwardRef((props, ref: any) => <Edit {...props} ref={ref} />),
-  Export: forwardRef((props, ref: any) => <SaveAlt {...props} ref={ref} />),
-  Filter: forwardRef((props, ref: any) => <FilterList {...props} ref={ref} />),
-  FirstPage: forwardRef((props, ref: any) => <FirstPage {...props} ref={ref} />),
-  LastPage: forwardRef((props, ref: any) => <LastPage {...props} ref={ref} />),
-  NextPage: forwardRef((props, ref: any) => <ChevronRight {...props} ref={ref} />),
-  PreviousPage: forwardRef((props, ref: any) => <ChevronLeft {...props} ref={ref} />),
-  ResetSearch: forwardRef((props, ref: any) => <Clear {...props} ref={ref} />),
-  Search: forwardRef((props, ref: any) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef((props, ref: any) => <ArrowDownward {...props} ref={ref} />),
-  ThirdStateCheck: forwardRef((props, ref: any) => <Remove {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref: any) => <ViewColumn {...props} ref={ref} />),
+    Add: forwardRef((props, ref: any) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref: any) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref: any) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref: any) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref: any) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref: any) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref: any) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref: any) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref: any) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref: any) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref: any) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref: any) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref: any) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref: any) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref: any) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref: any) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref: any) => <ViewColumn {...props} ref={ref} />),
 };
 
 const styles = createStyles({
-  root: {},
+    root: {},
 });
 
 interface Props extends WithStyles<typeof styles> {
-  printsStore: PrintsStore;
+    printsStore: PrintsStore;
 }
 
 @observer
 class AllPrintsTable extends Component<Props> {
-  render() {
-    const { classes, printsStore } = this.props;
+    render() {
+        const { classes, printsStore } = this.props;
 
-    return (
-      <div className={classes.root}>
-        <MaterialTable
-          title="Uploaded Prints"
-          columns={[
-            { title: "ID", field: "id", width: "auto" },
-            { title: "Print Name", field: "name", cellStyle: { wordBreak: "break-word" } },
-            { title: "Contact Details", field: "contactDetails" },
-            {
-              title: "Uploaded",
-              field: "timestamp",
-              render: (rowData) => moment(rowData.timestamp).fromNow(),
-              width: "auto",
-              // headerStyle: { width: "100px" },
-            },
-          ]}
-          data={printsStore.prints.map((p) => p)}
-          icons={tableIcons as Icons}
-          options={{
-            pageSize: 100,
-            pageSizeOptions: [20, 50, 100, 200],
-            emptyRowsWhenPaging: false,
-            // headerStyle: { width: "" },
-          }}
-          actions={[
-            {
-              icon: () => <PrintIcon />,
-              tooltip: "Send to 3D printer",
-              onClick: () => false,
-            },
-          ]}
-        />
-      </div>
-    );
-  }
+        return (
+            <div className={classes.root}>
+                <MaterialTable
+                    title="Uploaded Prints"
+                    columns={[
+                        { title: "#", field: "id", width: "auto" },
+                        { title: "Print Name", field: "name", cellStyle: { wordBreak: "break-word" } },
+                        { title: "Contact Details", field: "contactDetails" },
+                        {
+                            title: "Uploaded",
+                            field: "timestamp",
+                            render: (rowData) => moment(rowData.timestamp).fromNow(),
+                            width: "auto",
+                            defaultSort: "desc",
+                            // headerStyle: { width: "100px" },
+                        },
+                    ]}
+                    data={printsStore.prints.map((p) => p)}
+                    icons={tableIcons as Icons}
+                    options={{
+                        pageSize: 5,
+                        pageSizeOptions: [5, 10, 20, 50],
+                        emptyRowsWhenPaging: false,
+                        sorting: true,
+                        actionsColumnIndex: -1,
+                        // headerStyle: { width: "" },
+                    }}
+                    actions={[
+                        {
+                            icon: () => <PrintIcon />,
+                            tooltip: "Send to 3D printer",
+                            onClick: () => false,
+                        },
+                    ]}
+                />
+            </div>
+        );
+    }
 }
 
 export default withStyles(styles)(AllPrintsTable);
