46101322752582c88784c9502f91d23f193be6a5
Yarin <15931306+TheYarin@users.noreply.github.com>
2020-04-21 14:44:14 +0300

    Used the new TitleBar in UploadPage

--- client/mahaprinting-client/src/Components/Upload/UploadPage.tsx
+++ client/mahaprinting-client/src/Components/Upload/UploadPage.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Typography from "@material-ui/core/Typography";
 import { createStyles, WithStyles, withStyles } from "@material-ui/core";
 
 import UploadPrintForm from "./UploadPrintForm";
@@ -7,14 +6,13 @@ import MyPrints from "./MyPrints/MyPrints";
 import { ServerConnector } from "../../ServerAPI/ServerConnector";
 import { observer, Provider } from "mobx-react";
 import { UserPrintsStore } from "../../PrintStores/UserPrintsStore";
+import { flexCol, spaceChildren } from "../../JssUtils";
+import TitleBar from "../Common/TitleBar";
 
 const styles = createStyles({
   root: {
-    "& > *:not(:last-child)": {
-      marginBottom: 20,
-    },
-    display: "flex",
-    flexDirection: "column",
+    ...spaceChildren("vertically", 20),
+    ...flexCol,
     maxWidth: "min(600px, 100%)",
   },
   container: {
@@ -43,7 +41,7 @@ class UploadPage extends React.Component<Props> {
       <Provider userPrintStore={this.userPrintsStore}>
         <div className={classes.container}>
           <div className={classes.root}>
-            <Typography variant="h2">Upload</Typography>
+            <TitleBar title="Upload a new print" />
             <UploadPrintForm userPrintsStore={this.userPrintsStore} />
             {this.userPrintsStore.prints.length > 0 && <MyPrints userPrintsStore={this.userPrintsStore} />}
           </div>
