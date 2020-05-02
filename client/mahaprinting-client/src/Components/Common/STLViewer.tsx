04d21eac2f1787146612d9456ca8c43efd54092e
Yarin <15931306+TheYarin@users.noreply.github.com>
2020-05-02 18:34:55 +0300

    cleanup

--- client/mahaprinting-client/src/Components/Common/STLViewer.tsx
+++ client/mahaprinting-client/src/Components/Common/STLViewer.tsx
@@ -6,41 +6,41 @@ import { grey } from "@material-ui/core/colors";
 import OriginalSTLViewer from "stl-viewer";
 
 const styles = createStyles({
-    root: {},
+  root: {},
 });
 
 interface STLViewerProps extends WithStyles<typeof styles> {
-    stlFile: ArrayBuffer;
-    height?: number;
-    width?: number;
-    [x: string]: any;
-    theme: Theme;
+  stlFile: ArrayBuffer;
+  height?: number;
+  width?: number;
+  [x: string]: any;
+  theme: Theme;
 }
 
 class STLViewer extends Component<STLViewerProps> {
-    render() {
-        const { classes, stlFile, ...rest } = this.props;
+  render() {
+    const { classes, stlFile, ...rest } = this.props;
 
-        return (
-            <OriginalSTLViewer
-                model={stlFile}
-                width={300}
-                height={300}
-                modelColor={this.props.theme.palette.primary.main}
-                backgroundColor={grey[200]}
-                rotate={true}
-                orbitControls={true}
-                rotationSpeeds={[0, 0.02, 0]}
-                lights={[
-                    [-200, 0, 100],
-                    [100, -50, 100],
-                    [100, 0, -100],
-                    [0, 50, -50, -50],
-                ]}
-                {...rest}
-            />
-        );
-    }
+    return (
+      <OriginalSTLViewer
+        model={stlFile}
+        width={300}
+        height={300}
+        modelColor={this.props.theme.palette.primary.main}
+        backgroundColor={grey[200]}
+        rotate={true}
+        orbitControls={true}
+        rotationSpeeds={[0, 0.02, 0]}
+        lights={[
+          [-200, 0, 100],
+          [100, -50, 100],
+          [100, 0, -100],
+          [0, 50, -50, -50],
+        ]}
+        {...rest}
+      />
+    );
+  }
 }
 
 export default withStyles(styles, { withTheme: true })(STLViewer);
