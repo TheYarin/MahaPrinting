34e3a2092fde0037300a89ab34e5961b4936577b
Yarin <15931306+TheYarin@users.noreply.github.com>
2020-05-02 18:28:28 +0300

    CSS tweaks

--- client/mahaprinting-client/src/Components/Upload/UploadPage.tsx
+++ client/mahaprinting-client/src/Components/Upload/UploadPage.tsx
@@ -8,65 +8,59 @@ import Add from "@material-ui/icons/Add";
 import UploadNewPrint from "../Dialogs/UploadNewPrint";
 
 const styles = createStyles({
-    root: {
-        padding: 20,
-        maxWidth: 800,
-    },
-    uploadButton: {
-        position: "absolute",
-        zIndex: 1,
-        bottom: 0,
-        right: 0,
-        margin: 25,
-    },
-    container: {
-        width: "100%",
-        display: "flex",
-        justifyContent: "center",
-    },
+  root: {
+    maxWidth: 800,
+  },
+  uploadButton: {
+    position: "fixed",
+    zIndex: 1,
+    bottom: 0,
+    right: 0,
+    margin: 25,
+  },
+  container: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+  },
 });
 
 interface Props extends WithStyles<typeof styles> {
-    serverConnector: ServerConnector;
+  serverConnector: ServerConnector;
 }
 
 @observer
 class UploadPage extends React.Component<Props> {
-    state = {
-        uploadFormOpen: false,
-    };
+  state = {
+    uploadFormOpen: false,
+  };
 
-    _openUploadForm = () => this.setState({ uploadFormOpen: true });
-    _closeUploadForm = () => this.setState({ uploadFormOpen: false });
+  _openUploadForm = () => this.setState({ uploadFormOpen: true });
+  _closeUploadForm = () => this.setState({ uploadFormOpen: false });
 
-    userPrintsStore: UserPrintsStore = new UserPrintsStore(this.props.serverConnector);
+  userPrintsStore: UserPrintsStore = new UserPrintsStore(this.props.serverConnector);
 
-    async componentDidMount() {
-        await this.userPrintsStore.initialize();
-    }
+  async componentDidMount() {
+    await this.userPrintsStore.initialize();
+  }
 
-    render() {
-        const { classes } = this.props;
+  render() {
+    const { classes } = this.props;
 
-        return (
-            <Provider userPrintStore={this.userPrintsStore}>
-                <div className={classes.root}>
-                    {this.userPrintsStore.prints.length > 0 && <MyPrints userPrintsStore={this.userPrintsStore} />}
-                    <Fab
-                        color="primary"
-                        children={<Add />}
-                        className={classes.uploadButton}
-                        onClick={this._openUploadForm}
-                    />
-                    <UploadNewPrint
-                        userPrintsStore={this.userPrintsStore}
-                        open={this.state.uploadFormOpen}
-                        onClose={this._closeUploadForm}
-                    />
-                </div>
-            </Provider>
-        );
-    }
+    return (
+      <Provider userPrintStore={this.userPrintsStore}>
+        <div className={classes.root}>
+          {this.userPrintsStore.prints.length > 0 && <MyPrints userPrintsStore={this.userPrintsStore} />}
+          <Fab color="primary" children={<Add />} className={classes.uploadButton} onClick={this._openUploadForm} />
+          <UploadNewPrint
+            userPrintsStore={this.userPrintsStore}
+            open={this.state.uploadFormOpen}
+            onClose={this._closeUploadForm}
+          />
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default withStyles(styles)(UploadPage);
