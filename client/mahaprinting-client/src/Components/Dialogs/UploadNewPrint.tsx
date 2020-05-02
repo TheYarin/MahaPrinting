b51c56a0bd3915927369d8de5b487f643c3d593b
Yarin <15931306+TheYarin@users.noreply.github.com>
2020-05-02 22:50:55 +0300

    Improved readability of UploadNewPrint.tsx and fixed some tiny bugs

--- client/mahaprinting-client/src/Components/Dialogs/UploadNewPrint.tsx
+++ client/mahaprinting-client/src/Components/Dialogs/UploadNewPrint.tsx
@@ -2,110 +2,101 @@ import React, { Component, FormEvent } from "react";
 import { TextField, WithStyles, createStyles, withStyles, Button, Divider } from "@material-ui/core";
 import { flexCol, spaceChildren } from "../../JssUtils";
 import Dialog from "../Common/Dialog";
-import { observable } from "mobx";
+import { observable, computed } from "mobx";
 import { observer } from "mobx-react";
 import { UserPrintsStore } from "../../PrintStores/UserPrintsStore";
 import STLViewer from "../Common/STLViewer";
 
 const styles = createStyles({
-    root: {
-        ...spaceChildren("vertically", 20),
-        ...flexCol,
-        maxWidth: "min(600px, 100%)",
-    },
+  root: {
+    ...spaceChildren("vertically", 20),
+    ...flexCol,
+    maxWidth: "min(600px, 100%)",
+  },
 });
 
 interface Props extends WithStyles<typeof styles> {
-    open: boolean;
-    onClose: Function;
-    userPrintsStore: UserPrintsStore;
+  open: boolean;
+  onClose: Function;
+  userPrintsStore: UserPrintsStore;
 }
 
 @observer
 class UploadNewPrint extends Component<Props> {
-    state = {
-        canSubmitForm: false,
-    };
+  @observable name?: string;
+  @observable contactDetails?: string = window.localStorage["contactDetails"];
+  @observable notes?: string;
+  @observable file?: File;
+  fileInputRef: React.RefObject<HTMLInputElement> = React.createRef<HTMLInputElement>();
 
-    @observable name?: string;
-    @observable contactDetails?: string = window.localStorage["contactDetails"];
-    @observable notes?: string;
-    fileInput: React.RefObject<HTMLInputElement> = React.createRef<HTMLInputElement>();
+  @observable visited: { [key: string]: boolean } = {};
 
-    _checkCanSubmit = () => {
-        const printFile = this.fileInput.current!.files![0] as File;
+  @computed
+  get canSubmitForm() {
+    return this.name && this.contactDetails && this.file;
+  }
 
-        if (this.name && this.contactDetails && printFile) this.setState({ canSubmitForm: true });
-        else this.setState({ canSubmitForm: false });
-    };
+  submitPrint = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-    _onNameBlur = () => {
-        if (this.name === undefined) this.name = "";
-        else if (this.name) this._checkCanSubmit();
-    };
+    await this.props.userPrintsStore.add(this.name as string, this.contactDetails as string, this.file as File); //this.notes as string
 
-    _onContactDetailsBlur = () => {
-        if (this.contactDetails === undefined) this.contactDetails = "";
-        else if (this.contactDetails) this._checkCanSubmit();
-    };
+    window.localStorage["contactDetails"] = this.contactDetails;
+    this.resetForm();
+  };
 
-    submitPrint = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+  resetForm() {
+    this.name = undefined;
+    this.contactDetails = window.localStorage["contactDetails"];
+    this.notes = undefined;
+    this.file = undefined;
+    this.fileInputRef.current!.value = "";
+    this.visited = {};
+  }
 
-        const printFile = this.fileInput.current!.files![0] as File;
-        this.props.userPrintsStore.add(this.name as string, this.contactDetails as string, printFile); //this.notes as string
-
-        window.localStorage["contactDetails"] = this.contactDetails;
-
-        this.name = undefined;
-    };
-
-    render() {
-        const { classes } = this.props;
-
-        return (
-            <Dialog open={this.props.open} onClose={this.props.onClose} title="Upload New Print">
-                <>
-                    <form className={classes.root} autoComplete="off" onSubmit={this.submitPrint}>
-                        <TextField
-                            variant="outlined"
-                            name="name"
-                            label="Print name"
-                            value={this.name}
-                            onChange={(event) => (this.name = event.target.value)}
-                            error={this.name === ""}
-                            helperText="Required"
-                            onBlur={this._onNameBlur}
-                        />
-                        <TextField
-                            variant="outlined"
-                            name="contactDetails"
-                            label="Contact Details"
-                            placeholder="Your name, phone number..."
-                            value={this.contactDetails}
-                            onChange={(event) => (this.contactDetails = event.target.value)}
-                            error={this.contactDetails === ""}
-                            helperText="required"
-                            onBlur={this._onContactDetailsBlur}
-                        />
-                        <TextField
-                            variant="outlined"
-                            name="notes"
-                            label="Notes"
-                            placeholder="Any relevant information..."
-                            multiline
-                            value={this.notes}
-                            onChange={(event) => (this.notes = event.target.value)}
-                        />
-                        <input type="file" ref={this.fileInput} accept=".stl" onChange={this._checkCanSubmit} />
-                        <Button type="submit" variant="contained" color="primary" disabled={!this.state.canSubmitForm}>
-                            Upload print!
-                        </Button>
-                    </form>
-                </>
-            </Dialog>
-        );
-    }
+  render() {
+    const { classes } = this.props;
+    return (
+      <Dialog open={this.props.open} onClose={this.props.onClose} title="Upload New Print">
+        <form className={classes.root} autoComplete="off" onSubmit={this.submitPrint}>
+          <TextField
+            variant="outlined"
+            name="name"
+            label="Print name"
+            value={this.name || ""}
+            onChange={(event) => (this.name = event.target.value)}
+            error={this.visited["name"] && !this.name}
+            helperText="Required"
+            onBlur={() => (this.visited["name"] = true)}
+          />
+          <TextField
+            variant="outlined"
+            name="contactDetails"
+            label="Contact Details"
+            placeholder="Your name, phone number..."
+            value={this.contactDetails || ""}
+            onChange={(event) => (this.contactDetails = event.target.value)}
+            error={this.visited["contactDetails"] && !this.contactDetails}
+            helperText="required"
+            onBlur={() => (this.visited["contactDetails"] = true)}
+          />
+          <TextField
+            variant="outlined"
+            name="notes"
+            label="Notes"
+            placeholder="Any relevant information..."
+            multiline
+            value={this.notes || ""}
+            onChange={(event) => (this.notes = event.target.value)}
+          />
+          <input type="file" ref={this.fileInputRef} accept=".stl" onChange={(e) => (this.file = e.target.files?.[0])} />
+          <Button type="submit" variant="contained" color="primary" disabled={!this.canSubmitForm}>
+            Upload print!
+          </Button>
+        </form>
+      </Dialog>
+    );
+  }
 }
 
 export default withStyles(styles)(UploadNewPrint);
