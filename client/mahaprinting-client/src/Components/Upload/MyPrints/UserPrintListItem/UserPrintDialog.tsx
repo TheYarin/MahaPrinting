33f0de45a8651493fa35478f0fb1e5eea0d0c128
Yarin <15931306+TheYarin@users.noreply.github.com>
2020-05-02 18:57:58 +0300

    cleanup

--- client/mahaprinting-client/src/Components/Upload/MyPrints/UserPrintListItem/UserPrintDialog.tsx
+++ client/mahaprinting-client/src/Components/Upload/MyPrints/UserPrintListItem/UserPrintDialog.tsx
@@ -13,152 +13,146 @@ import { grey } from "@material-ui/core/colors";
 import moment from "moment";
 
 const styles = (theme: Theme) =>
-    createStyles({
-        root: {},
-        closeButton: {
-            // alignSelf: "flex-start",
-            float: "right",
-        },
-        dialogTitleRow: {
-            // display: "flex",
-            // flexDirection: "row",
-            // justifyContent: "space-between",
-        },
-        dialogTitle: {
-            // flexShrink: 1,
-            wordWrap: "break-word",
-        },
-        previewContainer: {
-            width: "100%",
-            ...flexColCentered,
-            backgroundColor: grey[200],
-            border: `1px solid ${grey[300]}`,
-            position: "relative",
-        },
-        previewText: {
-            fontStyle: "italic",
-            fontFamily: "monospace",
-            position: "absolute",
-            top: 0,
-            left: 0,
-            margin: 10,
-            fontSize: "105%",
-            color: grey[500],
-        },
-        printInfoDiv: {
-            margin: "10px 3px 0",
-        },
-        infoPairDiv: {
-            marginBottom: 10,
-            display: "flex",
-        },
-        infoPairTitle: {
-            marginRight: 5,
-            fontWeight: "bold",
-        },
-        infoPairContent: {
-            maxWidth: "50%",
-        },
-        button: {
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            float: "right",
-        },
-        buttonIcon: {
-            marginRight: 7,
-            fontSize: 16,
-        },
-    });
+  createStyles({
+    root: {},
+    closeButton: {
+      // alignSelf: "flex-start",
+      float: "right",
+    },
+    dialogTitleRow: {
+      // display: "flex",
+      // flexDirection: "row",
+      // justifyContent: "space-between",
+    },
+    dialogTitle: {
+      // flexShrink: 1,
+      wordWrap: "break-word",
+    },
+    previewContainer: {
+      width: "100%",
+      ...flexColCentered,
+      backgroundColor: grey[200],
+      border: `1px solid ${grey[300]}`,
+      position: "relative",
+    },
+    previewText: {
+      fontStyle: "italic",
+      fontFamily: "monospace",
+      position: "absolute",
+      top: 0,
+      left: 0,
+      margin: 10,
+      fontSize: "105%",
+      color: grey[500],
+    },
+    printInfoDiv: {
+      margin: "10px 3px 0",
+    },
+    infoPairDiv: {
+      marginBottom: 10,
+      display: "flex",
+    },
+    infoPairTitle: {
+      marginRight: 5,
+      fontWeight: "bold",
+    },
+    infoPairContent: {
+      maxWidth: "50%",
+    },
+    button: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      float: "right",
+    },
+    buttonIcon: {
+      marginRight: 7,
+      fontSize: 16,
+    },
+  });
 
 interface Props extends WithStyles<typeof styles> {
-    open: boolean;
-    onClose: Function; // This is intentionally not the same type as Dialog's onClose prop because that one really sucks
-    userPrint: UserPrint;
-    userPrintStore?: UserPrintsStore;
+  open: boolean;
+  onClose: Function; // This is intentionally not the same type as Dialog's onClose prop because that one really sucks
+  userPrint: UserPrint;
+  userPrintStore?: UserPrintsStore;
 }
 
 interface InfoPairProps {
-    title: ReactNode | string;
-    content: ReactNode | string;
+  title: ReactNode | string;
+  content: ReactNode | string;
 }
 
 @inject("userPrintStore")
 @observer
 class UserPrintDialog extends Component<Props> {
-    @observable stlFile?: ArrayBuffer;
+  @observable stlFile?: ArrayBuffer;
 
-    constructor(props: Props) {
-        super(props);
-        if (!props.userPrintStore)
-            throw new Error(
-                "Missing prop: myPrintStore." +
-                    " Yeah yeah, I know it's marked as optional, but that's just to trick typescript to work with Mobx's inject." +
-                    " It's actually mandatory. sorry."
-            );
-    }
+  constructor(props: Props) {
+    super(props);
+    if (!props.userPrintStore)
+      throw new Error(
+        "Missing prop: myPrintStore." +
+          " Yeah yeah, I know it's marked as optional, but that's just to trick typescript to work with Mobx's inject." +
+          " It's actually mandatory. sorry."
+      );
+  }
 
-    async componentDidMount() {
-        await this.loadStlFile();
-    }
+  async componentDidMount() {
+    await this.loadStlFile();
+  }
 
-    /* cancelPrint = () => {
+  /* cancelPrint = () => {
         const userAgreedToCancel = window.confirm("Are you sure you want to cancel this print?");
         if (userAgreedToCancel) this.props.userPrintStore!.cancelPrint(this.props.userPrint.id);
     }; */
 
-    async loadStlFile() {
-        const { userPrint, userPrintStore } = this.props;
-        try {
-            this.stlFile = await userPrintStore?.serverConnector.getPrintFile(userPrint.id);
-        } catch {
-            this.stlFile = undefined;
-        }
+  async loadStlFile() {
+    const { userPrint, userPrintStore } = this.props;
+    try {
+      this.stlFile = await userPrintStore?.serverConnector.getPrintFile(userPrint.id);
+    } catch {
+      this.stlFile = undefined;
     }
+  }
 
-    render() {
-        const { classes, userPrint, open, onClose } = this.props;
-        const { name, status, timestamp } = userPrint;
+  render() {
+    const { classes, userPrint, open, onClose } = this.props;
+    const { name, status, timestamp } = userPrint;
 
-        type GenericObject = { [key: string]: any };
+    type GenericObject = { [key: string]: any };
 
-        const printInfo = {
-            Uploaded: moment(timestamp).calendar(),
-            Status: status,
-            Notes: "<notes>",
-        } as GenericObject;
+    const printInfo = {
+      Uploaded: moment(timestamp).calendar(),
+      Status: status,
+      Notes: "<notes>",
+    } as GenericObject;
 
-        return (
-            <Dialog onClose={onClose} open={open} title={`Print: ${name}`}>
-                <div className={classes.previewContainer}>
-                    {this.stlFile && <STLViewer height="250" stlFile={this.stlFile} />}
-                    <Typography variant="caption" className={classes.previewText}>
-                        (STL Preview)
-                    </Typography>
-                </div>
+    return (
+      <Dialog onClose={onClose} open={open} title={`Print: ${name}`}>
+        <div className={classes.previewContainer}>
+          {this.stlFile && <STLViewer height={250} stlFile={this.stlFile} />}
+          <Typography variant="caption" className={classes.previewText}>
+            (STL Preview)
+          </Typography>
+        </div>
 
-                <div className={classes.printInfoDiv}>
-                    {Object.keys(printInfo).map((k) => (
-                        <Typography component="div" key={k} className={classes.infoPairDiv}>
-                            <div className={classes.infoPairTitle} children={`${k}:`} />
-                            <div className={classes.infoPairContent} children={printInfo[k]} />
-                        </Typography>
-                    ))}
-                    {status !== PrintStatus.CANCELED && (
-                        <Button
-                            className={classes.button}
-                            onClick={undefined}
-                            variant="contained"
-                            color={"primary"}
-                            size="small"
-                        >
-                            <DeleteIcon className={classes.buttonIcon} /> Delete Print
-                        </Button>
-                    )}
-                </div>
-            </Dialog>
-        );
-    }
+        <div className={classes.printInfoDiv}>
+          {Object.keys(printInfo).map((k) => (
+            <Typography component="div" key={k} className={classes.infoPairDiv}>
+              <div className={classes.infoPairTitle} children={`${k}:`} />
+              <div className={classes.infoPairContent} children={printInfo[k]} />
+            </Typography>
+          ))}
+          {status !== PrintStatus.CANCELED && (
+            <Button className={classes.button} onClick={undefined} variant="contained" color={"primary"} size="small">
+              <DeleteIcon className={classes.buttonIcon} /> Delete Print
+            </Button>
+          )}
+        </div>
+      </Dialog>
+    );
+  }
 }
 
 export default withStyles(styles)(UserPrintDialog);
