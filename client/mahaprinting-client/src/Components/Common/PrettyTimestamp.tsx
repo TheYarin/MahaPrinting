4ea150eede621d55203604b1e84a86654b74aa30
Yarin <15931306+TheYarin@users.noreply.github.com>
2020-05-15 23:30:44 +0300

    detailed timestamp is now shown in a tooltip when hovering over a print's prettified timestamp

---
+++ client/mahaprinting-client/src/Components/Common/PrettyTimestamp.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { createStyles, WithStyles, withStyles, Theme, Tooltip } from "@material-ui/core";
+import moment from "moment";
+
+const styles = (theme: Theme) =>
+  createStyles({
+    container: {
+      width: "fit-content",
+    },
+  });
+
+interface Props extends WithStyles<typeof styles> {
+  timestamp: string;
+}
+
+class PrettyTimestamp extends Component<Props> {
+  render() {
+    const { timestamp, classes } = this.props;
+
+    console.log("PrettyTimestamp -> render -> timestamp", timestamp);
+    const timestampMoment = moment(timestamp);
+    return (
+      <Tooltip title={timestampMoment.format("MMMM Do YYYY, HH:mm:ss")}>
+        <div className={classes.container}>{timestampMoment.fromNow()}</div>
+      </Tooltip>
+    );
+  }
+}
+
+export default withStyles(styles)(PrettyTimestamp);
--- client/mahaprinting-client/src/Components/Manage/PrintTables/FinishedPrintsTable.tsx
+++ client/mahaprinting-client/src/Components/Manage/PrintTables/FinishedPrintsTable.tsx
@@ -5,56 +5,54 @@ import RemoveIcon from "@material-ui/icons/Delete";
 import StyledPrintsTable from "../../Common/StyledPrintsTable";
 import { PrintsStore } from "../../../PrintStores/PrintsStore";
 import { observer } from "mobx-react";
-import moment from "moment";
 import * as muiColors from "@material-ui/core/colors";
 import { PrintStatus } from "../../../ServerAPI/PrintStatus";
 import { Column } from "material-table";
+import PrettyTimestamp from "../../Common/PrettyTimestamp";
 
 const styles = createStyles({
-    icon: { color: muiColors.grey[700] },
+  icon: { color: muiColors.grey[700] },
 });
 
 interface Props extends WithStyles<typeof styles> {
-    printsStore: PrintsStore;
+  printsStore: PrintsStore;
 }
 
 @observer
 class FinishedPrintsTable extends Component<Props> {
-    render() {
-        const { classes, printsStore } = this.props;
-        console.log(printsStore.prints);
-        return (
-            <StyledPrintsTable
-                title={"Print History"}
-                columns={[
-                    { title: "Print Status", field: "status", width: "18%" } as Column<any>,
-                    {
-                        title: "Printed",
-                        field: "timestamp_printed",
-                        render: (rowData) => moment(rowData.timestamp).fromNow(),
-                        width: "10%",
-                    } as Column<any>,
-                    {
-                        title: "Uploaded",
-                        field: "timestamp",
-                        render: (rowData) => moment(rowData.timestamp).fromNow(),
-                        width: "10%",
-                    } as Column<any>,
-                    { title: "Contact Details", field: "contactDetails", width: "25%" } as Column<any>,
-                ]}
-                data={printsStore.prints
-                    .filter((p) => [PrintStatus.DONE, PrintStatus.CANCELED].includes(p.status))
-                    .map((p) => p)}
-                actions={[
-                    {
-                        icon: () => <RemoveIcon className={classes.icon} />,
-                        tooltip: "Delete Print Log",
-                        onClick: () => false,
-                    },
-                ]}
-            />
-        );
-    }
+  render() {
+    const { classes, printsStore } = this.props;
+    console.log(printsStore.prints);
+    return (
+      <StyledPrintsTable
+        title={"Print History"}
+        columns={[
+          { title: "Print Status", field: "status", width: "18%" } as Column<any>,
+          {
+            title: "Printed",
+            field: "timestamp_printed",
+            render: (rowData) => <PrettyTimestamp timestamp={rowData.timestamp} />,
+            width: "10%",
+          } as Column<any>,
+          {
+            title: "Uploaded",
+            field: "timestamp",
+            render: (rowData) => <PrettyTimestamp timestamp={rowData.timestamp} />,
+            width: "10%",
+          } as Column<any>,
+          { title: "Contact Details", field: "contactDetails", width: "25%" } as Column<any>,
+        ]}
+        data={printsStore.prints.filter((p) => [PrintStatus.DONE, PrintStatus.CANCELED].includes(p.status)).map((p) => p)}
+        actions={[
+          {
+            icon: () => <RemoveIcon className={classes.icon} />,
+            tooltip: "Delete Print Log",
+            onClick: () => false,
+          },
+        ]}
+      />
+    );
+  }
 }
 
 export default withStyles(styles)(FinishedPrintsTable);
