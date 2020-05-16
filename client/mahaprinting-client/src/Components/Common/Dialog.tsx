f53795fbadc9d10d75ec0f8d040a23c8d3c8f689
Yarin <15931306+TheYarin@users.noreply.github.com>
2020-05-16 22:07:11 +0300

    Fix CustomDialog width

--- client/mahaprinting-client/src/Components/Common/Dialog.tsx
+++ client/mahaprinting-client/src/Components/Common/Dialog.tsx
@@ -5,50 +5,44 @@ import CloseIcon from "@material-ui/icons/Close";
 import TitleBar from "./TitleBar";
 
 const styles = (theme: Theme) =>
-    createStyles({
-        root: {
-            borderTopLeftRadius: 0,
-            borderTopRightRadius: 0,
-            minWidth: 400,
-        },
-        closeButton: {
-            padding: 2,
-            marginLeft: 20,
-            marginRight: 10,
-            color: "white",
-        },
-        childrenWrapper: {
-            padding: 15,
-        },
-    });
+  createStyles({
+    root: {
+      borderTopLeftRadius: 0,
+      borderTopRightRadius: 0,
+      minWidth: "min(400px,100% - 10px)",
+    },
+    closeButton: {
+      padding: 2,
+      marginLeft: 20,
+      marginRight: 10,
+      color: "white",
+    },
+    childrenWrapper: {
+      padding: 15,
+    },
+  });
 
 interface Props extends WithStyles<typeof styles> {
-    title: string;
-    open: boolean;
-    onClose: any;
+  title: string;
+  open: boolean;
+  onClose: any;
 }
 
 @observer
 class CustomDialog extends Component<Props> {
-    render() {
-        const { classes, children, title, ...otherProps } = this.props;
+  render() {
+    const { classes, children, title, ...otherProps } = this.props;
 
-        return (
-            <Dialog {...otherProps} classes={{ paper: classes.root }}>
-                <TitleBar
-                    title={title}
-                    iconSection={
-                        <IconButton
-                            className={classes.closeButton}
-                            children={<CloseIcon />}
-                            onClick={this.props.onClose}
-                        />
-                    }
-                />
-                <div className={classes.childrenWrapper}>{children}</div>
-            </Dialog>
-        );
-    }
+    return (
+      <Dialog {...otherProps} classes={{ paper: classes.root }}>
+        <TitleBar
+          title={title}
+          iconSection={<IconButton className={classes.closeButton} children={<CloseIcon />} onClick={this.props.onClose} />}
+        />
+        <div className={classes.childrenWrapper}>{children}</div>
+      </Dialog>
+    );
+  }
 }
 
 export default withStyles(styles)(CustomDialog);
