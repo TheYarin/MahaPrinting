6abe4c4ebbe80b02f0198349a44bd2b18ff295bc
Yarin <15931306+TheYarin@users.noreply.github.com>
2020-09-06 23:29:26 +0300

    Dockerized the app

--- client/mahaprinting-client/src/App.tsx
+++ client/mahaprinting-client/src/App.tsx
@@ -12,66 +12,61 @@ import { WithStyles, createStyles, withStyles, Theme } from "@material-ui/core";
 import { Toolbar, Typography } from "@material-ui/core";
 
 const styles = (theme: Theme) =>
-    createStyles({
-        appRoot: { backgroundColor: "whitesmoke", height: "100%", minHeight: "100vh" },
-        appBar: { backgroundColor: theme.palette.global.appBarColor, color: "white", padding: "7px 12px" },
-        appTitle: { fontWeight: "bold", fontFamily: "monospace", marginLeft: 15 },
-        pageRoot: {},
-    });
+  createStyles({
+    appRoot: { backgroundColor: "whitesmoke", height: "100%", minHeight: "100vh" },
+    appBar: { backgroundColor: theme.palette.global.appBarColor, color: "white", padding: "7px 12px" },
+    appTitle: { fontWeight: "bold", fontFamily: "monospace", marginLeft: 15 },
+    pageRoot: {},
+  });
 
 type Props = RouteComponentProps<{}> & WithStyles<typeof styles>;
 
 @observer
 class App extends Component<Props> {
-    serverConnector?: ServerConnector;
-    @observable initialized: boolean = false;
+  serverConnector?: ServerConnector;
+  @observable initialized: boolean = false;
 
-    async componentDidMount() {
-        this.serverConnector = new ServerConnector("http://localhost:5000");
-        await this.serverConnector.initialize();
+  async componentDidMount() {
+    this.serverConnector = new ServerConnector("http://localhost/api");
+    await this.serverConnector.initialize();
 
-        this.initialized = true;
-    }
+    this.initialized = true;
+  }
 
-    _returnHome = () => this.props.history.push("/");
+  _returnHome = () => this.props.history.push("/");
 
-    render() {
-        if (this.initialized === false) return "";
+  render() {
+    if (this.initialized === false) return "";
 
-        const { classes } = this.props;
-        const currentPath = this.props.location.pathname;
+    const { classes } = this.props;
+    const currentPath = this.props.location.pathname;
 
-        const castServerConnector = this.serverConnector as ServerConnector;
+    const castServerConnector = this.serverConnector as ServerConnector;
 
-        return (
-            <div className={classes.appRoot}>
-                <Toolbar className={classes.appBar}>
-                    <Typography
-                        variant="h4"
-                        className={classes.appTitle}
-                        style={{ cursor: "pointer" }}
-                        onClick={this._returnHome}
-                    >
-                        MahaPrinting
-                    </Typography>
-                    <Typography variant="h5" className={classes.appTitle}>
-                        {`// ${currentPath !== "/" ? currentPath.split("/")[1] : "home"}`}
-                    </Typography>
-                </Toolbar>
-                <div className={classes.pageRoot}>
-                    <Switch>
-                        <Route exact path="/">
-                            <UploadPage serverConnector={castServerConnector} />
-                        </Route>
-                        <Route path="/manage">
-                            <ManagePage serverConnector={castServerConnector} />
-                        </Route>
-                        <Redirect to="/" />
-                    </Switch>
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div className={classes.appRoot}>
+        <Toolbar className={classes.appBar}>
+          <Typography variant="h4" className={classes.appTitle} style={{ cursor: "pointer" }} onClick={this._returnHome}>
+            MahaPrinting
+          </Typography>
+          <Typography variant="h5" className={classes.appTitle}>
+            {`// ${currentPath !== "/" ? currentPath.split("/")[1] : "home"}`}
+          </Typography>
+        </Toolbar>
+        <div className={classes.pageRoot}>
+          <Switch>
+            <Route exact path="/">
+              <UploadPage serverConnector={castServerConnector} />
+            </Route>
+            <Route path="/manage">
+              <ManagePage serverConnector={castServerConnector} />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default withRouter(withStyles(styles)(App));
